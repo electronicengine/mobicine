1c32b808b95381c61182c3c4a1e192d47d6f9cc5
Yusuf Bülbül <electronicengine@users.noreply.github.com>
2020-12-20 18:14:32 +0300

    Update mobicine.php

--- rest_api/mobicine.php
+++ rest_api/mobicine.php
@@ -1,22 +1,32 @@
 <?php
 
-
-// Takes raw data from the request
+//$json = "{\n    \"FunctionName\": \"getPossibleDiseases\",\n    \"params\": [\n        \"Failure to thrive\"     ]\n}\n";
 $json = file_get_contents('php://input');
-
-// Converts it into a PHP object
 $data = json_decode($json);
 
+$function_name = $data->{"FunctionName"};
+$params = $data->{"params"};
+
 $service = new MobicineService();
+$service->accessService($function_name, $params); //result: ACANTHOSIS NIGRICANS
+
+
 
 //$service->accessService("getSimilarDiseases", "ACANTHOSIS");  // result: ACANTHOSIS NIGRICANS
-//$service->accessService("getSimilarSymptoms", "acanthosis");  //result: Benign acanthosis nigricans
+//$service->accessService("getSimilarSymptoms", "Abdominal aortic aneurysm");  //result: Benign acanthosis nigricans
 //$service->accessService("getCodesFromSymptom", "Thick hyperpigmented flexural area skin"); //result: 100600
-//$service->accessService("getCodesFromDisease", "ACANTHOSIS NIGRICANS"); //result: 100600
+//$service->accessService("getCodeFromDisease", "ACANTHOSIS NIGRICANS"); //result: 100600
 //$service->accessService("getSymptomsFromCode", 100600); //result: Benign acanthosis nigricans, Thick hyperpigmented flexural area skin
-$service->accessService("getDiseasesFromCode", 100600); //result: ACANTHOSIS NIGRICANS
+//$service->accessService("getDiseaseFromCode", 100600); //result: ACANTHOSIS NIGRICANS
 
-// $query = $data->query;
+//$diseases = array("AORTIC ANEURYSM, FAMILIAL ABDOMINAL, 1; AAA1", "AORTIC ANEURYSM, FAMILIAL THORACIC 8; AAT8");
+//$symptoms = array("Failure to thrive","Seizure", "Hyperreflexia"); 
+//$service->accessService("getSameSymptoms", $diseases);
+//$service->accessService("getDifferentialSymptoms", $diseases);
+//$service->accessService("getPossibleDiseases", $symptoms);
+//$service->accessService("getDifferentialDiseases", $symptoms);
+
+
 
 class MobicineService
 {
@@ -27,7 +37,7 @@ class MobicineService
     private $symptom_column = "symptom";
     private $disease_column = "disease";
     private $code_column = "code";
-    private $max_line  = 100;
+    private $max_line  = 600;
 
 
     function accessService($FunctionName, $Params)
@@ -37,34 +47,34 @@ class MobicineService
         switch($FunctionName)
         {
             case 'getSimilarDiseases';
-
-                $result = $this->getSimilarDiseases($Params);
+				
+                $result = $this->getSimilarDiseases($Params[0]);
 
                 break;
             case 'getSimilarSymptoms';
 
-                $result = $this->getSimilarSymptoms($Params);
+                $result = $this->getSimilarSymptoms($Params[0]);
 
                 break;
             case 'getCodesFromSymptom';
 
-                $result = $this->getCodesFromSymptom($Params);
+                $result = $this->getCodesFromSymptom($Params[0]);
 
                 break;
-            case 'getCodesFromDisease';
+            case 'getCodeFromDisease';
 
-                $result = $this->getCodesFromDisease($Params);
+                $result = $this->getCodeFromDisease($Params[0]);
 
                 break;
             case 'getSymptomsFromCode';
 
-                $result = $this->getSymptomsFromCode($Params);
+                $result = $this->getSymptomsFromCode($Params[0]);
 
                 break;
 
-            case 'getDiseasesFromCode';
+            case 'getDiseaseFromCode';
 
-                $result = $this->getDiseasesFromCode($Params);
+                $result = $this->getDiseaseFromCode($Params[0]);
 
                 break;
 
@@ -83,9 +93,9 @@ class MobicineService
                 $result = $this->getDifferentialDiseases($Params);
 
                 break;
-            case 'getSameDiseases';
+            case 'getPossibleDiseases';
 
-                $result = $this->getSameDiseases($Params);
+                $result = $this->getPossibleDiseases($Params);
 
                 break;
 
@@ -148,7 +158,6 @@ class MobicineService
     function getSimilarSymptoms($SymptomHint)
     {
         $qry = "SELECT ".$this->symptom_column." FROM ".$this->symptom_table." WHERE ".$this->symptom_column." LIKE '%".$SymptomHint."%'";
-
         return $this->getSearchResult($qry, $this->symptom_column);
     }
 
@@ -167,7 +176,7 @@ class MobicineService
     {
         $result_list = array();
 
-        $qry = "SELECT ".$this->code_column." FROM ".$this->symptom_table." WHERE ".$this->symptom_column." LIKE '%".$Symptom."%'";
+        $qry = "SELECT ".$this->code_column." FROM ".$this->symptom_table." WHERE ".$this->symptom_column." LIKE '".$Symptom."%'";
 
         $arr = $this->getSearchResult($qry, $this->code_column);
         $uniq_arr = array_unique($arr);
@@ -178,23 +187,21 @@ class MobicineService
 
 
 
-    function getCodesFromDisease($Disease)
+    function getCodeFromDisease($Disease)
     {
         $result_list = array();
 
-        $qry = "SELECT ".$this->code_column." FROM ".$this->disease_table." WHERE ".$this->disease_column." LIKE '%".$Disease."%'";
+        $qry = "SELECT ".$this->code_column." FROM ".$this->disease_table." WHERE ".$this->disease_column." LIKE '".$Disease."'";
 
         $arr = $this->getSearchResult($qry, $this->code_column);
-        $uniq_arr = array_unique($arr);
-
-        return $uniq_arr;
+		
+        return $arr[0];
     }
 
 
 
     function getSymptomsFromCode($Code)
     {
-
         $result_list = array();
 
         $qry = "SELECT ".$this->symptom_column." FROM ".$this->symptom_table." WHERE ".$this->code_column." = ".$Code;
@@ -203,72 +210,69 @@ class MobicineService
         $uniq_arr = array_unique($arr);
 
         return $uniq_arr;
-
     }
 
 
 
-    function getDiseasesFromCode($Code)
+    function getDiseaseFromCode($Code)
     {
         $result_list = array();
 
         $qry = "SELECT ".$this->disease_column." FROM ".$this->disease_table." WHERE ".$this->code_column." = ".$Code;
 
         $arr = $this->getSearchResult($qry, $this->disease_column);
-        $uniq_arr = array_unique($arr);
 
-        return $uniq_arr;
+        return $arr[0];
     }
 
 
 
     function getDifferentialSymptoms($DiseaseList)
     {
-        $result_list = array();
-        $all_symptom_list = array();
+		$all_symptoms = array();
 
         foreach ($DiseaseList as &$Disease)
         {
-            $disease_code_list = $this->getCodesFromDisease($Disease);
+            $disease_code = $this->getCodeFromDisease($Disease);
+			$symptoms = $this->getSymptomsFromCode($disease_code);
 
-            foreach ($disease_code_list as &$code)
-            {
-                $symptom_list = $this->getSymptomsFromCode($code);
-
-                array_merge($all_symptom_list, $symptom_list);
-            }
+			$this->merge($all_symptoms, $symptoms);
         }
+				
+		$disease_count = sizeof($DiseaseList);
 
-        $withoutDuplicates = array_unique($all_symptom_list);
+		if($disease_count == 1)
+			return $all_symptoms;
+		
+		$repeated_symptoms= $this->extractRepeatedValues($all_symptoms ,$disease_count);
+		$uniq_symptoms = array_unique($all_symptoms);
+		$result = array_diff_assoc($uniq_symptoms, $repeated_symptoms);
 
-        return $withoutDuplicates;
-
+		return $result;
     }
 
 
 
     function getSameSymptoms($DiseaseList)
     {
-        $result_list = array();
-        $all_symptom_list = array();
+		
+		$all_symptoms = array();
 
-        foreach ($DiseaseList as &$Disease)
+		foreach ($DiseaseList as &$Disease)
         {
-            $disease_code_list = $this->getCodesFromDisease($Disease);
+			
+   			$disease_code = $this->getCodeFromDisease($Disease);
+			$symptoms = $this->getSymptomsFromCode($disease_code);
 
-            foreach ($disease_code_list as &$code)
-            {
-                $symptom_list = $this->getSymptomsFromCode($code);
-
-                array_merge($all_symptom_list, $symptom_list);
-            }
+			$this->merge($all_symptoms, $symptoms);
+			
         }
 
-        $withoutDuplicates = array_unique($all_symptom_list);
-        $duplicates = array_diff($all_symptom_list, $withoutDuplicates);
-        $uniq_dublicates = array_unique($duplicates);
+		$disease_count = sizeof($DiseaseList);
 
-        return $uniq_dublicates;
+		$repeated_symptoms= $this->extractRepeatedValues($all_symptoms ,$disease_count);
+
+		return $repeated_symptoms;
 
     }
 
@@ -279,63 +283,98 @@ class MobicineService
 
         $result_list = array();
         $all_disease_list = array();
+		$all_code_list = array();
+		$result_list = array();
 
         foreach ($SymptomList as &$symptom)
         {
             $symptom_code_list = $this->getCodesFromSymptom($symptom);
+			$this->merge($all_code_list, $symptom_code_list);
 
-            foreach ($symptom_code_list as &$code)
-            {
-                $diseases_list = $this->getDiseasesFromCode($code);
-
-                array_merge($all_disease_list, $diseases_list);
-            }
         }
 
-        $withoutDuplicates = array_unique($all_disease_list);
+		$symptom_count = sizeof($SymptomList);
+		
+		$repeated_codes = $this->extractRepeatedValues($all_code_list ,$symptom_count);
+		$uniq_codes = array_diff_assoc($all_code_list, $repeated_codes);
+		
+		foreach ($uniq_codes as &$code)
+		{
+			$disease = $this->getDiseaseFromCode($code);
+			$result_list[] = $disease;
+		}
 
-        return $withoutDuplicates;
+		return $result_list;
+		
 
     }
 
 
 
-    function getSameDiseases($SymptomList)
+    function getPossibleDiseases($SymptomList)
     {
         $result_list = array();
         $all_disease_list = array();
-
+		$all_code_list = array();
+		$result_list = array();
+		
         foreach ($SymptomList as &$symptom)
         {
             $symptom_code_list = $this->getCodesFromSymptom($symptom);
 
-            foreach ($symptom_code_list as &$code)
-            {
-                $diseases_list = $this->getDiseasesFromCode($code);
-
-                array_merge($all_disease_list, $diseases_list);
-            }
+			$this->merge($all_code_list, $symptom_code_list);
         }
 
-        $withoutDuplicates = array_unique($all_disease_list);
-        $duplicates = array_diff($all_disease_list, $withoutDuplicates);
-        $uniq_dublicates = array_unique($duplicates);
+		$symptom_count = sizeof($SymptomList);
 
-        return $uniq_dublicates;
+		$repeated_codes = $this->extractRepeatedValues($all_code_list ,$symptom_count);
 
+		foreach ($repeated_codes as $code)
+		{
+			$disease = $this->getDiseaseFromCode($code);
+			$result_list[] = $disease;
+		}
+
+		return $result_list;
     }
+	
+	
+	
+	function merge(&$array1, &$array2) 
+	{
+		foreach($array2 as $i) 
+		{
+			$array1[] = $i;
+		}
+	}
+	
+	
+	
+	function extractRepeatedValues(&$Array, $Count) 
+	{				
+		$counts = array_count_values($Array);
+		$Count--;
 
+		$repeated_title  = array_filter($Array, function ($value) use ($counts, $Count) {
+			return $counts[$value] > $Count;
+		});
+            		
+  		$repeated_title = array_unique($repeated_title);
 
+		return $repeated_title;
+  
+	}
 }
+
 
 
 
 class DataBase
 {
-  private $servername = "******";
-  private $database = "******";
-  private $username = "******";
-  private $password = "*****";
+  private $servername = "";
+  private $database = "";
+  private $username = "";
+  private $password = "";
   private $conn;
 
   function open()
